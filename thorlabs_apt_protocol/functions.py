0ec76fe83dbebdc6f4f45930ca22f88a44e5ead1
Kyle Sunden <git@ksunden.space>
2020-06-05 02:44:54 -0500

    Fill out many more functions

--- thorlabs_apt_protocol/functions.py
+++ thorlabs_apt_protocol/functions.py
@@ -8,8 +8,169 @@ def _pack(msgid: int, dest: int, source: int, *, param1: int=0, param2: int=0, d
     else:
         return struct.pack("<H4b", msgid, param1, param2, dest, source)
 
-def mot_move_home(dest: int, source: int, chan_ident: int):
+def mod_identify(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0223, dest, source, param1=chan_ident)
+
+def mod_set_chanenablestate(dest: int, source: int, chan_ident: int, enable_state:int) -> bytes:
+    return _pack(0x0210, dest, source, param1=chan_ident, param2=enable_state)
+
+def mod_req_chanenablestate(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0211, dest, source, param1=chan_ident)
+
+def hw_disconnect(dest: int, source: int) -> bytes:
+    return _pack(0x0002, dest, source)
+
+def hw_start_updatemsgs(dest: int, source: int) -> bytes:
+    return _pack(0x0011, dest, source)
+
+def hw_stop_updatemsgs(dest: int, source: int) -> bytes:
+    return _pack(0x0012, dest, source)
+
+def hw_req_info(dest: int, source: int) -> bytes:
+    return _pack(0x0005, dest, source)
+
+def rack_req_bayused(dest: int, source: int, bay_ident: int) -> bytes:
+    return _pack(0x0060, dest, source, param1=bay_ident)
+
+def hub_req_bayused(dest: int, source: int) -> bytes:
+    return _pack(0x0065, dest, source)
+
+def rack_req_statusbits(dest: int, source: int) -> bytes:
+    # I suspect there is an error in the docs, and status_bits should be omitted
+    # This reflects what I think it _should_ be
+    # - KFS 2020-06-05
+    return _pack(0x0226, dest, source)
+
+def rack_set_digoutputs(dest: int, source: int, dig_outs: Sequence[Bool]) -> bytes:
+    dig_out_param = 0
+    bit = 1
+    for i in dig_outs:
+        if i:
+            dig_out_param |= bit
+        bit <<= 1
+    return _pack(0x0228, dest, source, param1=dig_out_param)
+
+def rack_get_digoutputs(dest: int, source: int) -> bytes:
+    return _pack(0x0229, dest, source)
+
+def mod_set_digoutputs(dest: int, source: int, chan_ident: int) -> bytes:
+    dig_out_param = 0
+    bit = 1
+    for i in dig_outs:
+        if i:
+            dig_out_param |= bit
+        bit <<= 1
+    return _pack(0x0213, dest, source, param1=dig_out_param)
+
+def mod_req_digoutputs(dest: int, source: int) -> bytes:
+    # I suspect there is an error in the docs, and bits should be omitted
+    # This reflects what I think it _should_ be
+    # - KFS 2020-06-05
+    return _pack(0x0214, dest, source)
+
+def hw_set_kcubemmilock(dest: int, source: int, mmi_lock: int) -> bytes:
+    return _pack(0x0250, dest, source, param2=mmi_lock)
+
+def hw_req_kcubemmilock(dest: int, source: int) -> bytes:
+    # I suspect there is an error in the docs, and bits should be omitted
+    # This reflects what I think it _should_ be
+    # - KFS 2020-06-05
+    return _pack(0x0251, dest, source)
+
+def restorefactorysettings(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0686, dest, source, param1=chan_ident)
+
+def hw_yes_flash_programming(dest: int, source: int) -> bytes:
+    return _pack(0x0017, dest, source)
+
+def hw_no_flash_programming(dest: int, source: int) -> bytes:
+    return _pack(0x0018, dest, source)
+
+def mot_set_poscounter(dest: int, source: int, chan_ident: int, position: int) -> bytes:
+    data = struct.pack("<Hl", chan_ident, position)
+    return _pack(0x0410, dest, source, data=data)
+
+def mot_req_poscounter(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0411, dest, source, param1=chan_ident)
+
+def mot_set_enccounter(dest: int, source: int, chan_ident: int, encoder_count) -> bytes:
+    data = struct.pack("<Hl", chan_ident, encoder_count)
+    return _pack(0x0409, dest, source, data=data)
+
+def mot_req_enccounter(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x040A, dest, source, param1=chan_ident)
+
+def mot_set_velparams(dest: int, source: int, chan_ident: int, min_velocity: int, acceleration: int, max_velocity: int) -> bytes:
+    data = struct.pack("<H3l", chan_ident, min_velocity, acceleration, max_velocity)
+    return _pack(0x0413, dest, source, data=data)
+
+def mot_req_velparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0414, dest, source, param1=chan_ident)
+
+def mot_set_jogparams(dest: int, source: int, chan_ident: int, jog_mode: int, step_size: int, min_velocity:int , acceleration:int , max_velocity:int , stop_mode: int) -> bytes:
+    data = struct.pack("<HH4lH", chan_ident, jog_mode, step_size, min_velocity, acceleration, max_velocity, stop_mode)
+    return _pack(0x0416, dest, source, param1=chan_ident)
+
+def mot_req_jogparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0417, dest, source, param1=chan_ident)
+
+def mot_req_adcinputs(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x042B, dest, source, param1=chan_ident)
+
+def mot_set_powerparams(dest: int, source: int, chan_ident: int, rest_factor: int, move_factor: int) -> bytes:
+    data = struct.pack("<3H", chan_ident, rest_factor, move_factor)
+    return _pack(0x0426, dest, source, data=data)
+
+def mot_req_powerparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0427, dest, source, param1=chan_ident)
+
+def mot_set_genmoveparams(dest: int, source: int, chan_ident: int, backlash_distance: int) -> bytes:
+    data = struct.pack("<Hl", chan_ident, backlash_distance)
+    return _pack(0x043A, dest, source, data=data)
+
+def mot_req_genmoveparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x043B, dest, source, param1=chan_ident)
+
+def mot_set_moverelparams(dest: int, source: int, chan_ident: int, relative_distance: int) -> bytes:
+    data = struct.pack("<Hl", chan_ident, relative_distance)
+    return _pack(0x0445, dest, source, data=data)
+
+def mot_req_moverelparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0446, dest, source, param1=chan_ident)
+
+
+def mot_set_moveabsparams(dest: int, source: int, chan_ident: int, absolute_position: int) -> bytes:
+    data = struct.pack("<Hl", chan_ident, absolute_position)
+    return _pack(0x0450, dest, source, data=data)
+
+def mot_req_moveabsparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0451, dest, source, param1=chan_ident)
+
+def mot_set_homeparams(dest: int, source: int, chan_ident: int, home_dir: int, limit_switch: int, home_velocity: int, offset_distance: int) -> bytes:
+    data = struct.pack("<3Hll", chan_ident, home_dir, limit_switch, home_velocity, offset_distance)
+    return _pack(0x0440, dest, source, param1=chan_ident)
+
+def mot_req_homeparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0441, dest, source, param1=chan_ident)
+
+def mot_set_limswitchparams(dest: int, source: int, chan_ident: int, cw_hardlimit: int, ccw_hardlimit: int, cw_softlimit: int, ccw_softlimit: int, sort_limit_mode: int) -> bytes:
+    data = struct.pack("<3HLLH", chan_ident, cw_hardlimit, ccw_hardlimit, cw_softlimit, ccw_softlimit, soft_limit_mode)
+    return _pack(0x0423, dest, source, data=data)
+
+def mot_req_limswitchparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x0424, dest, source, param1=chan_ident)
+
+def mot_move_home(dest: int, source: int, chan_ident: int) -> bytes:
     return _pack(0x0443, dest, source, param1=chan_ident)
+
+def mot_move_relative(dest: int, source: int, chan_ident: int, distance: Optional[int]=None):
+    msgid = 0x0448
+    if distance is None:
+        return _pack(msgid, dest, source, param1=chan_ident)
+    else:
+        data = struct.pack("<Hl", chan_ident, distance)
+        return _pack(msgid, dest, source, data=data)
+
 
 def mot_move_absolute(dest: int, source: int, chan_ident: int, position: Optional[int]=None):
     msgid = 0x0453
@@ -19,12 +180,68 @@ def mot_move_absolute(dest: int, source: int, chan_ident: int, position: Optiona
         data = struct.pack("<Hl", chan_ident, position)
         return _pack(msgid, dest, source, data=data)
 
-def hw_yes_flash_programming(dest: int, source: int):
-    return _pack(0x0017, dest, source)
+def mot_move_jog(dest: int, source: int, chan_ident: int, direction) -> bytes:
+    return _pack(0x046A, dest, source, param1=chan_ident, param2=direction)
 
-def hw_no_flash_programming(dest: int, source: int):
-    return _pack(0x0018, dest, source)
+def mot_move_velocity(dest: int, source: int, chan_ident: int, direction) -> bytes:
+    return _pack(0x0457, dest, source, param1=chan_ident, param2=direction)
 
-def mot_ack_dcstatusupdate(dest: int, source: int):
+def mot_move_stop(dest: int, source: int, chan_ident: int, stop_mode: int) -> bytes:
+    return _pack(0x0465, dest, source, param1=chan_ident, param2=stop_mode)
+
+def mot_set_bowindex(dest: int, source: int, chan_ident: int, bow_index: int) -> bytes:
+    data = struct.pack("<HH", chan_ident, bow_index)
+    return _pack(0x04F4, dest, source, param1=chan_ident)
+
+def mot_req_bowindex(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x04F5, dest, source, param1=chan_ident)
+
+def mot_set_dcpidparams(dest: int, source: int, chan_ident: int, proportional: Optional[int]=None, integral: Optional[int]=None, differential: Optional[int]=None, integral_limit: Optional[int]=None) -> bytes:
+    filter_control = 0
+    if proportional is not None:
+        filter_control |= 1
+    else:
+        proportional = 0
+    if integral is not None:
+        filter_control |= 2
+    else:
+        integral = 0
+    if differential is not None:
+        filter_control |= 4
+    else:
+        differential = 0
+    if integral_limit is not None:
+        filter_control |= 8
+    else:
+        integral_limit = 0
+    data = struct.pack("<H4LH", chan_ident, proportional, integral, differential, integral_limits, filter_control)
+    return _pack(0x04A0, dest, source, data=data)
+
+def mot_req_dcpidparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x04A1, dest, source, param1=chan_ident)
+
+def mot_set_avmodes(dest: int, source: int, chan_ident: int, mode_bits: int) -> bytes:
+    data = struct.pack("<HH", chan_ident, mode_bits)
+    return _pack(0x04B3, dest, source, data=data)
+
+def mot_req_avmodes(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x04B4, dest, source, param1=chan_ident)
+
+def mot_set_potparams(dest: int, source: int, chan_ident: int, zero_wnd: int, vel1: int, wnd1: int, vel2:int, wnd2: int, vel3:int, vel4:int) -> bytes:
+    data = struct.pack("<HHlHlHlHl", chan_ident, zero_wnd, vel1, wnd1, vel2, wnd2, vel3, wnd3, vel4)
+    return _pack(0x04B0, dest, source, data=data)
+
+def mot_req_potparams(dest: int, source: int, chan_ident: int) -> bytes:
+    return _pack(0x04B1, dest, source, param1=chan_ident)
+
+def mot_set_buttonparams(dest: int, source: int, chan_ident: int, mode: int, position1: int, position2: int, time_out1: int, time_out2: int) -> bytes:
+    data = struct.pack("<HHllHH", chan_ident, mode, position1, position2, time_out1, time_out2)
+    return _pack(0x04B6, dest, source, data=data)
+
+def mot_req_buttonparams(dest: int, source: int, chan_ident: int, msgid_param: int) -> bytes:
+    data = struct.pack("<HH", chan_ident, msgid_param)
+    return _pack(0x04B9, dest, source, data=data)
+
+def mot_ack_dcstatusupdate(dest: int, source: int) -> bytes:
     return _pack(0x0492, dest, source)
 
--- thorlabs_apt_protocol/parsing.py
+++ thorlabs_apt_protocol/parsing.py
@@ -1,5 +1,6 @@
 import struct
 import functools
+from typing import Dict, Any
 
 id_to_func = {}
 HEADER_SIZE = 6
@@ -7,18 +8,20 @@ HEADER_SIZE = 6
 def parser(msgid):
     def wrapper(func):
         @functools.wraps(func)
-        def inner(data: bytes):
+        def inner(data: bytes) -> Dict[str, Any]:
             msgid_read, _, dest, source = struct.unpack_from("<HHBB", data)
             dest = dest & ~0x80
             assert msgid == msgid_read
             ret = {"msg": func.__name__, "msgid": msgid, "dest": dest, "source": source}
             ret.update(func(data))
             return ret
+        if msgid in id_to_func:
+            raise ValueError(f"Duplicate msgid: {hex(msgid)}")
         id_to_func[msgid] = inner
         return inner
     return wrapper
 
-def _parse_dcstatus(data: bytes):
+def _parse_dcstatus(data: bytes) -> Dict[str, Any]:
     chan_ident, position, velocity, _, status_bits = struct.unpack_from("<HlHHL", data, HEADER_SIZE)
     ret = {
             "chan_ident": chan_ident,
@@ -28,7 +31,8 @@ def _parse_dcstatus(data: bytes):
     ret.update(_parse_status_bits(status_bits))
     return ret
 
-def _parse_status_bits(status_bits: int):
+def _parse_status_bits(status_bits: int) -> Dict[str, Any]:
+    # Bitfield
     return {
             "forward_limit_switch": bool(status_bits & 0x1),
             "reverse_limit_switch": bool(status_bits & 0x2),
@@ -45,26 +49,204 @@ def _parse_status_bits(status_bits: int):
             "channel_enabled": bool(status_bits & 0x80000000),
             }
 
+@parser(0x0212)
+def mod_get_chanenablestate(data: bytes) -> Dict[str, Any]:
+    return {"chan_ident": data[2], "enabled": data[3] == 0x01}
 
-@parser(0x0491)
-def mot_get_dcstatusupdate(data: bytes):
-    return _parse_dcstatus(data)
+@parser(0x0002)
+def hw_disconnect(data: bytes) -> Dict[str, Any]:
+    return {}
 
-@parser(0x0464)
-def mot_move_completed(data: bytes):
-    return _parse_dcstatus(data)
+@parser(0x0080)
+def hw_response(data: bytes) -> Dict[str, Any]:
+    return {}
+
+@parser(0x0081)
+def hw_richresponse(data: bytes) -> Dict[str, Any]:
+    msg_ident, code, notes = struct.unpack_from("<HH64c", data, HEADER_SIZE)
+    return {"msg_ident": msg_ident, "code": code, "notes": notes}
+
+@parser(0x0006)
+def hw_get_info(data: bytes) -> Dict[str, Any]:
+    serial_number, model_number, type_, firmware_version*,_, _, hw_version, mod_state, nchs = struct.unpack_from("<l8cH4B60sHHH", data, HEADER_SIZE)
+    return {"serial_number": serial_number,
+            "model_number": model_number,
+            "type": type_,
+            "firmware_version": firmware_version,
+            "hw_version": hw_version,
+            "mod_state": mod_state,
+            "nchs": nchs,
+            }
+
+@parser(0x0061)
+def rack_get_bayused(data: bytes) -> Dict[str, Any]:
+    return {"bay_ident": data[2], "occupied": data[3] == 0x01}
+
+@parser(0x0066)
+def hub_get_bayused(data: bytes) -> Dict[str, Any]:
+    bay_ident = data[2]
+    if bay_ident == 0xff:
+        bay_ident = -1
+    return {"bay_ident": bay_ident}
+
+@parser(0x0226)
+def rack_get_statusbits(data: bytes) -> Dict[str, Any]:
+    # Bitfield
+    status_bits = struct.unpack_from("<L", data, HEADER_SIZE)
+    return {
+            "digouts": [bool(status_bits & 0x1),
+            bool(status_bits & 0x2),
+            bool(status_bits & 0x3),
+            bool(status_bits & 0x4),
+            ]
+            }
+
+@parser(0x0230)
+def rack_get_digoutputs(data: bytes) -> Dict[str, Any]:
+    # Bitfield
+    return {
+            "digouts": [bool(data[2] & 0x1),
+            bool(data[2] & 0x2),
+            bool(data[2] & 0x3),
+            bool(data[2] & 0x4),
+            ]
+            }
+
+@parser(0x0215)
+def mod_get_digoutputs(data: bytes) -> Dict[str, Any]:
+    # This differs from 0x0225 and 0x0230, as the number of outputs is not known
+    # Bitfield
+    return {"bits": data[2]}
+
+@parser(0x0252)
+def hw_get_kcubemmilock(data: bytes) -> Dict[str, Any]:
+    return {"locked": data[3] == 0x01}
+
+@parser(0x0412)
+def mot_get_poscounter(data: bytes) -> Dict[str, Any]:
+    chan_ident, position = struct.unpack_from("<Hl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "position": position}
+
+@parser(0x040B)
+def mot_get_enccounter(data: bytes) -> Dict[str, Any]:
+    chan_ident, encoder_count = struct.unpack_from("<Hl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "encoder_count": encoder_count}
+
+@parser(0x0415)
+def mot_get_velparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, min_velocity, acceleration, max_velocity = struct.unpack_from("<H3l", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "min_velocity": min_velocity, "acceleration": acceleration, "max_velocity": max_velocity}
+
+@parser(0x0418)
+def mot_get_jogparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, jog_mode, step_size, min_velocity, acceleration, max_velocity, stop_mode = struct.unpack_from("<HH4lH", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident,
+            "jog_mode": jog_mode,
+            "step_size": step_size,
+            "min_velocity": min_velocity,
+            "acceleration": acceleration,
+            "max_velocity": max_velocity,
+            "stop_mode": stop_mode,
+            }
+
+@parser(0x042C)
+def mot_get_adcinputs(data: bytes) -> Dict[str, Any]:
+    adc_input1, adc_input2 = struct.unpack_from("<HH", data, HEADER_SIZE)
+    return {"adc_input1": adc_input1 * 5 / 2**15, "adc_input2": adc_input2 * 5 / 2 ** 15}
+
+@parser(0x0428)
+def mot_get_powerparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, rest_factor, move_factor = struct.unpack_from("<3H", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "rest_factor": rest_factor, "move_factor": move_factor}
+
+@parser(0x043C)
+def mot_genmoveparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, backlash_distance = struct.unpack_from("<Hl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "backlash_distance": backlash_distance}
+
+@parser(0x0447)
+def mot_get_moverelparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, relative_distance = struct.unpack_from("<Hl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "relative_distance": relative_distance}
+
+@parser(0x0447)
+def mot_get_moveabsparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, absolute_position = struct.unpack_from("<Hl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "absolute_position": absolute_position}
+
+@parser(0x0442)
+def mot_get_homeparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, home_dir, limit_switch, home_velocity, offset_distance = struct.unpack_from("<3Hll", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "home_dir": home_dir, "limit_switch": limit_switch, "home_velocity": home_velocity, "offset_distance": offset_distance}
+
+@parser(0x0425)
+def mot_get_limswitchparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, cw_hardlimit, ccw_hardlimit, cw_softlimit, ccw_softlimit, soft_limit_mode = struct.unpack_from("<3HLLH", data, HEADER_SIZE)
+    return {
+            "chan_ident": chan_ident,
+            "cw_hardlimit": cw_hardlimit,
+            "ccw_hardlimit": ccw_hardlimit,
+            "cw_softlimit": cw_softlimit,
+            "ccw_softlimit": ccw_softlimit,
+            "soft_limit_mode": soft_limit_mode,
+            }
 
 @parser(0x0444)
-def mot_move_homed(data: bytes):
+def mot_move_homed(data: bytes) -> Dict[str, Any]:
     return {"chan_ident": data[2]}
 
+@parser(0x0464)
+def mot_move_completed(data: bytes) -> Dict[str, Any]:
+    return _parse_dcstatus(data)
+
+@parser(0x0466)
+def mot_move_stopped(data: bytes) -> Dict[str, Any]:
+    return _parse_dcstatus(data)
+
+@parser(0x04F6)
+def mot_get_bowindex(data: bytes) -> Dict[str, Any]:
+    chan_ident, bow_index = struct.unpack_from("<HH", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "bow_index": bow_index}
+
+@parser(0x04A2)
+def mot_get_dcpidparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, proportional, integral, differential, integral_limits, filter_control = struct.unpack_from("<H4LH", data, HEADER_SIZE)
+    return {"chan_ident": data[2], "enabled": data[3] == 0x01}
+
+@parser(0x04B5)
+def mot_get_avmodes(data: bytes) -> Dict[str, Any]:
+    chan_ident, mode_bits = struct.unpack_from("<HH", data, HEADER_SIZE)
+    # Bitfield
+    return {"chan_ident": chan_ident, "mode_bits": mode_bits}
+
+@parser(0x04B2)
+def mot_get_potparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, zero_wnd, vel1, wnd1, vel2, wnd2, vel3, wnd3, vel4 = struct.unpack_from("<HHlHlHlHl", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident, "zero_wnd": zero_wnd, "vel1": vel1, "wnd1": wnd1, "vel2": vel2, "wnd2": wnd2, "vel3": vel3, "wnd3": wnd3, "vel4": vel4}
+
+@parser(0x04B8)
+def mot_get_buttonparams(data: bytes) -> Dict[str, Any]:
+    chan_ident, mode, position1, position2, time_out1, time_out2 = struct.unpack_from("<HHllHH", data, HEADER_SIZE)
+    return {"chan_ident": chan_ident,
+            "mode": mode,
+            "position1": position1,
+            "position2": position2,
+            "time_out1": time_out1,
+            "time_out2": time_out2,
+            }
+
+@parser(0x0491)
+def mot_get_dcstatusupdate(data: bytes) -> Dict[str, Any]:
+    return _parse_dcstatus(data)
+
 @parser(0x042A)
-def mot_get_statusbits(data: bytes):
+def mot_get_statusbits(data: bytes) -> Dict[str, Any]:
     chan_ident, status_bits = struct.unpack_from("<HL", data, HEADER_SIZE)
     ret = {
             "chan_ident": chan_ident,
             }
     ret.update(_parse_status_bits(status_bits))
+    # Bitfield
     return ret
     
 
